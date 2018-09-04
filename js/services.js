d890a1fedb11dbbbf63a50f346613463ff0b0f7a
Conceição <igor.a.conceicao@accenture.com>
2018-09-04 16:56:26 -0300

    Construindo página de Login

---
+++ js/services.js
@@ -0,0 +1,21 @@
+var services = {
+	settings : {
+		"async": true,
+		"url": "",
+		"type": "POST",
+		"data": ""
+	},
+	settingsGet : {
+		"async": true,
+		"url": "",
+		"type": "GET"
+	},
+	logar : function(obejct, callback) {
+		services.settings.url = "https://reqres.in/api/login";
+		services.settings.data = object;
+
+		$.ajax(services.settings).done(function(response) {
+			callback(response);
+		});
+	}
+}
