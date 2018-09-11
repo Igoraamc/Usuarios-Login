1b2fb74d1c19bf9bc1d4366f78959ae80b325ef2
Conceição <igor.a.conceicao@accenture.com>
2018-09-11 10:25:24 -0300

    Alterando maneira de pegar usuarios

--- js/login.js
+++ js/login.js
@@ -77,10 +77,8 @@ let getAllUsers = function() {
 		console.log(data);
 
 		for (var i = 0; i < data.data.length; i++) {
-			console.log(data.data[i]);
 			user = data.data[i];
-			console.log(user);
-			users_array.push(user+";");
+			users_array.push(user);
 		}
 	}
 
@@ -88,7 +86,6 @@ let getAllUsers = function() {
 
 	for (var i = 0; i < 4; i++) {
 		services.listaDeUsuarios((i + 1), addUsersToArray);
-		console.log('Entrou '+i);
 	}
 
 	return users_array;
